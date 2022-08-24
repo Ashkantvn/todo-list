336cf452496a4bed5e35b27af50ca6b3a9a0a4f9
AshkanTavangar <ashkantavangar7@gmail.com>
2022-08-24 15:43:37 +0430

    Create event functions and test of input component

---
+++ src/parts/input/input.test.tsx
@@ -0,0 +1,26 @@
+import { render, fireEvent } from "@testing-library/react";
+import Input from "./input";
+
+describe(Input, () => {
+  it("Should change notesInput after textarea changed", () => {
+    const { getByTestId } = render(<Input />);
+    const textareaOfInput = getByTestId("textarea-of-input");
+
+    fireEvent.change(textareaOfInput, { target: { value: "something" } });
+    expect(textareaOfInput.innerHTML).toContain("something");
+  });
+
+  it("Submit validation", () => {
+    const { getByRole, getByTestId } = render(<Input />);
+    const textareaOfInput = getByTestId("textarea-of-input");
+    const submitButton = getByRole("button", { name: "Submit" });
+    global.alert = jest.fn();
+
+    fireEvent.click(submitButton);
+    expect(global.alert).toHaveBeenCalledTimes(1);
+
+    fireEvent.change(textareaOfInput, { target: { value: "something" } });
+    fireEvent.click(submitButton);
+    expect(textareaOfInput.innerHTML).toHaveLength(0);
+  });
+});
--- src/parts/input/input.tsx
+++ src/parts/input/input.tsx
@@ -1,12 +1,45 @@
 import "./style/input.css";
+import { useState, useContext, ChangeEvent, MouseEventHandler } from "react";
+import { context } from "../../context/context";
+
 const Input = () => {
+  const [notesInput, setNotesInput] = useState<string>("");
+  const getContext = useContext(context);
+
+  ////function of onchange to add value of textarea to notesOfinput
+  const textareaHandler: (e: ChangeEvent<HTMLTextAreaElement>) => void = (
+    e
+  ) => {
+    setNotesInput((notesInput) => (notesInput = e.target.value));
+    console.log(e.target.innerHTML.length);
+  };
+
+  /////function of submit button  for pushing notesOfInput to notes of context and validation
+  const submitHandler: MouseEventHandler = (e) => {
+    e.preventDefault();
+    if (notesInput) {
+      setNotesInput((notesInput) => (notesInput = ""));
+    } else {
+      alert("fill empty parts");
+    }
+  };
+
   return (
     <>
       <form>
         <h1>ToDo-list</h1>
         <label htmlFor="textarea">Enter note :</label>
-        <textarea name="" id="textarea" cols={30} rows={5} />
-        <button type="submit">Submit</button>
+        <textarea
+          onChange={textareaHandler}
+          data-testid="textarea-of-input"
+          id="textarea"
+          cols={30}
+          rows={5}
+          value={notesInput}
+        ></textarea>
+        <button onClick={submitHandler} type="submit">
+          Submit
+        </button>
         <button type="reset">Reset</button>
       </form>
     </>
