a81a3b205528fc7f01d3b662dbdab122fa891067
AshkanTavangar <ashkantavangar7@gmail.com>
2022-08-24 20:23:09 +0430

    create buttons of item functions and edit style

--- src/parts/item/item.tsx
+++ src/parts/item/item.tsx
@@ -1,16 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import "./style/item.css"
 import { propsOfItem } from "../../context/interface";
 
 
 const Item :FC<propsOfItem>= ({note}) => {
+  const [success , setSuccess] = useState<boolean>(false);
+  const [forget , setforget] = useState<boolean>(false);
+  const [del , setDel] = useState<boolean>(false);
+  
+  const styleOfMainDiv:()=>string = ()=>{
+    if(success){
+      return "item success";
+    }if(forget){
+      return "item danger";
+    }if(del){
+      return "del-item";
+    }else{
+      return "item";
+    }
+  }
+
   return (
     <>
-      <div className="item">
+      <div className={styleOfMainDiv()}>
         <p data-testid="note-of-props">{note}</p>
-        <button className="success-button">.</button>
-        <button className="del-button" >.</button>
-        <button className="danger-button">.</button>
+        <button onClick={()=>{setSuccess(success=>!success)}} className="success">Success</button>
+        <button onClick={()=>{setDel(success=>!success)}} className="del" >Delete</button>
+        <button onClick={()=>{setforget(forget=>!forget)}}className="danger">Forget</button>
       </div>
     </>
   );
