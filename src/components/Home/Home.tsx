158cf937c383f35d225bc32851389843908a1802
AshkanTavangar <ashkantavangar7@gmail.com>
2022-08-24 18:50:28 +0430

    Mapping data from context

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,20 +1,26 @@
+import { useContext } from "react";
+import { context } from "../../context/context";
 import Input from "../../parts/input/input";
 import Item from "../../parts/item/item";
 import "./style/Home.css";
 
 const Home = () => {
+  const getnotes = useContext(context);
+
+  const mappedData = getnotes?.contextValue.notes.map((item) => {
+    return (
+      <>
+        <Item note={item} />
+      </>
+    );
+  });
+
   return (
     <>
       <header>
-        <Input/>
+        <Input />
       </header>
-      <div className="list">
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-      </div>
+      <div className="list">{mappedData}</div>
     </>
   );
 };
